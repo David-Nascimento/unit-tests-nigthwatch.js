0271c993978bb8ae22db1684639d45c30db67460
David Halison <halison700@gmail.com>
2021-04-12 22:04:55 -0300

    [UPDATE] implementado cenarios de testes para as credenciais

--- tests/login/sem_sucesso.test.js
+++ tests/login/sem_sucesso.test.js
@@ -10,6 +10,31 @@ module.exports = {
         .setValue(senha, 'admin')
         .click(btnLogar)
         .assert.containsText('#spanMessage', "Invalid credentials")
+    },
+
+    'email nao informado': (browser) => {
+        let login = '#txtUsername'
+        let senha = '#txtPassword'
+        let btnLogar = '#btnLogin'
+        browser
+        .url('https://opensource-demo.orangehrmlive.com/')
+        .waitForElementVisible('#divLogo', 3000)
+        .setValue(login, '')
+        .setValue(senha, 'admin')
+        .click(btnLogar)
+        .assert.containsText('#spanMessage', "Username cannot be empty")
+    },
+    'senha nao informado': (browser) => {
+        let login = '#txtUsername'
+        let senha = '#txtPassword'
+        let btnLogar = '#btnLogin'
+        browser
+        .url('https://opensource-demo.orangehrmlive.com/')
+        .waitForElementVisible('#divLogo', 3000)
+        .setValue(login, 'Admin')
+        .setValue(senha, '')
+        .click(btnLogar)
+        .assert.containsText('#spanMessage', "Password cannot be empty")
         .end();
     }
 }
