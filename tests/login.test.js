6239b55cdf7290b2bdcc2fe3d3c27ed546bcc024
David Halison <halison700@gmail.com>
2021-04-12 21:13:04 -0300

    [UPDATE] exemplos de steps e multi-steps

---
+++ tests/login.test.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+    'login com sucesso': function(browser) {
+        let login = '#txtUsername'
+        let senha = '#txtPassword'
+        let btnLogar = '#btnLogin'
+        browser
+        .url('https://opensource-demo.orangehrmlive.com/')
+        .waitForElementVisible('#divLogo', 3000)
+        .setValue(login, 'Admin')
+        .setValue(senha, 'admin123')
+        .click(btnLogar)
+        .assert.containsText('#welcome', 'Welcome Paul')
+        .end();
+    },
+
+    'quando eu acesso a pagina de login': function(browser) {
+        browser
+        .url('https://opensource-demo.orangehrmlive.com/')
+        .waitForElementVisible('#divLogo', 3000)
+    },
+
+    'e faco login com sucesso': function(browser) {
+        let login = '#txtUsername'
+        let senha = '#txtPassword'
+        let btnLogar = '#btnLogin'
+        browser
+        .setValue(login, 'Admin')
+        .setValue(senha, 'admin123')
+        .click(btnLogar)
+    },
+
+    'entao devo ver a area logada': function(browser){
+        browser
+        .waitForElementVisible('#welcome')
+        .assert.containsText('#welcome', 'Welcome Paul')
+        .end();
+    }
+    
+}
