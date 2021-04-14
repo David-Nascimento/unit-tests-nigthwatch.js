84631bb710744e7da4518dac7077cefcb3cdc925
David Halison <halison700@gmail.com>
2021-04-13 21:45:25 -0300

    [BDD] Criando cenarios em BDD

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -8,7 +8,7 @@ const chromedriver = require('chromedriver');
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['tests'],
+  src_folders: ['tests/cadastro'],
 
   // See https://nightwatchjs.org/guide/working-with-page-objects/
   page_objects_path: './pages',
---
+++ tests/cadastro/novo_funcionario.test.js
@@ -0,0 +1,20 @@
+let cadastroFuncionario = {}
+
+module.exports = {
+    'Dado que eu tenho um novo funcionario para cadastro': function(browser) {
+        cadastroFuncionario = {
+            name: 'David',
+            lasName: 'Nascimento',
+            address: 'Rua 70',
+            emailAdress: 'testes@testes.com.br',
+            phone: '85 99419-4958',
+            pass: '123123123',
+            confirPass: '123123123',
+            cover: 'img1.jpg'
+        }
+    },
+
+    'Quando eu faco o cadastro do novo funcionario': function(browser) {
+        console.log(cadastroFuncionario)
+    }
+}
